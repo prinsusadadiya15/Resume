2bacacabcacf504853d5037ed937fca5d6e59ad4
prins <prinsusadadiya210@gmail.com>
2025-10-16 09:37:01 +0530

    contact page change

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,46 +1,100 @@
-import React from 'react'
+import { useState } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-
+import "./Contact.css"
 
 const Contact = () => {
-    return (
-        <>
-            {/* navbar  */}
-            <Navbar />
 
-            <div className='bg-[#e6dace] pt-[150px] pb-[70px]'>
-                <div className=' text-[30px] font-bold text-center pt-[60px] pb-[60px]'>
-                    <li className='list-[square] list-inside text-[blue] text-[35px]'><span className='text-black '>Let's talk</span></li>
-                </div>
+  let [formdata, setFormdata] = useState({
+    name: "",
+    lname: "",
+    email: "",
+    sub: "",
+    message: ""
+  })
 
-                <div className="flex justify-center">
-                    <div className='bg-white h-[600px] w-[600px] p-[60px] font-bold text-[13px] shadow-lg shadow-black'>
-                        <label htmlFor="">First Name*</label>
-                        <label htmlFor="" className='ms-[180px]' >Last Name*</label><br /><br /><br />
+  let changeInput = () => {
+    let olddata = { ...formdata }
+    let name = event.target.name;
+    let val = event.target.value;
 
-                        <input type="text" className='inp' required />
-                        <input type="text" className='inp1' required /> <br />
+    olddata[name] = val;
 
-                        <label htmlFor="" className='pt-[30px]'>Email*</label> <br /><br /><br />
-                        <input type="text" className='inp w-[420px]' required /> <br />
+    setFormdata(olddata)
 
-                        <label htmlFor="" className='pt-[30px]'>Subject*</label> <br /><br /><br />
-                        <input type="text" className='inp w-[420px]' required /><br />
+  }
 
-                        <label htmlFor="" className='pt-[30px]'>Message*</label> <br /><br /><br /><br /><br />
-                        <input type="text" className='inp w-[420px]' required />
+  let send = (event) => {
+    event.preventDefault();
 
-                        <button className='mt-3 px-8 py-2.5 btn1'>Send</button>
-                    </div>
-                </div>
+
+    if (formdata.name === '' || formdata.lname === '' || formdata.email === '' || formdata.sub === '' ||formdata.message === '' )
+    {
+      alert("Some Data is not Found")
+    }else
+    {
+      alert("You Internet Connection is lost")
+    }
+  
+}
+
+
+  return (
+    <>
+      <Navbar />
+      <div className="container-fluid bg-[#E6DACE] pb-[100px]">
+        <div className='flex justify-center items-center py-[70px]'>
+          <li className='list-[square] text-[blue] text-[54px] pt-[100px]'></li>
+          <span className='text-[38px] text-black font-bold pt-[100px]'>Let's talk</span>
+        </div>
+        <div className="container-small py-[70px] px-[40px] bg-white">
+          <form action="" className='font-bold text-[14px]'>
+            {/* Frist row */}
+            <div className="flex flex-col sm:flex-row justify-between">
+              <div className='sm:w-[45%] sm:ps-[20px]'>
+                <label htmlFor="name">Frist Name*</label>
+                <br />
+                <br />
+                <input type="text" className='input1 w-[100%]' name='name' value={formdata.name} onChange={changeInput} />
+              </div>
+              <div className='sm:w-[45%] pt-[20px] sm:pe-[20px] sm:py-[0px]'>
+                <label htmlFor="lname">Last Name*</label>
+                <br />
+                <br />
+                <input type="text" className='input1 w-[100%]' name="lname" value={formdata.lname} onChange={changeInput} />
+              </div>
             </div>
-
-            {/* footer  */}
-            <Footer />
-
-        </>
-    )
+            <br />
+            {/* Other Rows */}
+            <div className='sm:px-[20px]'>
+              <label htmlFor="lname">Email*</label>
+              <br />
+              <br />
+              <input type="Email" className='input1 w-[100%]' name="email" value={formdata.email} onChange={changeInput} />
+            </div>
+            <br />
+            <div className='sm:px-[20px]'>
+              <label htmlFor="lname">Subject*</label>
+              <br />
+              <br />
+              <input type="text" className='input1 w-[100%]' name="sub" vlaue={formdata.sub} onChange={changeInput} />
+            </div>
+            <br />
+            <div className='sm:px-[20px]'>
+              <label htmlFor="lname">Message*</label>
+              <br />
+              <br />
+              <textarea type="text" className='input1 w-[100%]' name="message" value={formdata.message} onChange={changeInput}>
+              </textarea>
+            </div>
+            <br />
+            <button className='btn1' onClick={send}>Send</button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Contact
